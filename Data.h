bdd0d434e7e8981f72608053c8fe52a23b519578
frescante <frescante@users.noreply.github.com>
2018-09-12 15:03:44 -0400

    Added Header file

---
+++ Data.h
@@ -0,0 +1,17 @@
+#ifndef DATA_H
+define DATA_H
+class Data
+{
+	private;
+			int x;
+			float y;
+	public;
+			Data();
+			Data (int a, float b);
+			int getX();
+			void setX(int a);
+			float getY();
+			void setY(float a);
+			
+};
+#endif
